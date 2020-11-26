62d0eec33d89624541d16b0b62ef0df8058f9bc0
Dominick Baier <dbaier@leastprivilege.com>
2020-11-26 10:36:25 +0100

    sample1 code complete

---
+++ IdentityServer/v5/SharedApis/ResourceBasedApi/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Client;
+using IdentityModel.AspNetCore.AccessTokenValidation;
+
+namespace ResourceBasedApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors();
+            services.AddDistributedMemoryCache();
+
+            services.AddAuthentication("token")
+
+                // JWT tokens
+                .AddJwtBearer("token", options =>
+                {
+                    options.Authority = Urls.IdentityServer;
+                    options.Audience = "resource1";
+
+                    options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+
+                    // if token does not contain a dot, it is a reference token
+                    options.ForwardDefaultSelector = Selector.ForwardReferenceToken("introspection");
+                })
+
+                // reference tokens
+                .AddOAuth2Introspection("introspection", options =>
+                {
+                    options.Authority = Urls.IdentityServer;
+
+                    options.ClientId = "resource1";
+                    options.ClientSecret = "secret";
+                });
+
+            services.AddScopeTransformation();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers().RequireAuthorization();
+            });
+        }
+    }
+}
---
+++ IdentityServer/v5/SharedApis/SimpleApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Client;
+
+namespace SampleApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors();
+            services.AddDistributedMemoryCache();
+
+            // this API will accept any access token from the authority
+            services.AddAuthentication("token")
+                .AddJwtBearer("token", options =>
+                {
+                    options.Authority = Urls.IdentityServer;
+                    options.TokenValidationParameters.ValidateAudience = false;
+                    
+                    options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers().RequireAuthorization();
+            });
+        }
+    }
+}
