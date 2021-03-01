50f801f822f5812cdeea53f804caf01ee47f0fd8
Dominick Baier <dbaier@leastprivilege.com>
2021-03-01 11:34:25 +0100

    add WebForms client

---
+++ IdentityServer/v5/Clients/WebForms/WebForms/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForms._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <dl>
+
+    <% 
+        var user = User as System.Security.Claims.ClaimsPrincipal;
+
+        foreach (var claim in user.Claims)
+        {
+    %>
+   
+            <dt>
+                <%: claim.Type %>
+            </dt>
+
+            <dd>
+                <%: claim.Value %>
+            </dd>
+        
+    <%
+        }
+    %>
+
+    </dl>
+
+</asp:Content>
---
+++ IdentityServer/v5/Clients/WebForms/WebForms/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+[assembly: OwinStartup(typeof(WebForms.Startup))]
+
+namespace WebForms
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = "Cookies",
+                ExpireTimeSpan = TimeSpan.FromMinutes(10),
+                SlidingExpiration = true
+            });
+
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                AuthenticationType = "oidc",
+                SignInAsAuthenticationType = "Cookies",
+
+                Authority = "https://demo.duendesoftware.com",
+                
+                ClientId = "login",
+
+                RedirectUri = "https://localhost:44306/",
+                PostLogoutRedirectUri = "https://localhost:44306/",
+
+                ResponseType = "id_token",
+                Scope = "openid profile",
+                UseTokenLifetime = false
+            });
+
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+    }
+}
