f8cc5861d622b9d3feb73be03ee2a91b1b2acbce
Brock Allen <brockallen@gmail.com>
2021-04-01 17:28:37 -0400

    add ASP.NET Identity sample w/ out of box AspId template

---
+++ IdentityServer/v5/AspNetIdentitySample/Client/Pages/Secure.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    [Authorize]
+    public class SecureModel : PageModel
+    {
+        private readonly ILogger<SecureModel> _logger;
+
+        public SecureModel(ILogger<SecureModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
