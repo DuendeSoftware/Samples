d2f1ca90dba5e696965bac033e99e2d345ba8cb1
Dominick Baier <dbaier@leastprivilege.com>
2021-04-16 15:20:25 +0200

    cleanup

--- IdentityServer/v5/BFF/BlazorServer/src/BlazorServer/Startup.cs
+++ IdentityServer/v5/BFF/BlazorServer/src/BlazorServer/Startup.cs
@@ -80,7 +80,6 @@ namespace BlazorServer
                 });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -90,11 +89,8 @@ namespace BlazorServer
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
