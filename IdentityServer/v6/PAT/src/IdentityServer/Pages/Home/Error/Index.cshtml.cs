8b36a2315539d704d2db45861290c5a716949536
Brock Allen <brockallen@gmail.com>
2022-01-04 14:47:40 -0500

    update some samples to latest UI pages for v6

--- IdentityServer/v6/PAT/src/IdentityServer/Pages/Home/Error/Index.cshtml.cs
+++ IdentityServer/v6/PAT/src/IdentityServer/Pages/Home/Error/Index.cshtml.cs
@@ -1,40 +1,41 @@
 using System.Threading.Tasks;
 using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Hosting;
 
-namespace IdentityServerHost.Pages.Error
+namespace IdentityServerHost.Pages.Error;
+
+[AllowAnonymous]
+[SecurityHeaders]
+public class Index : PageModel
 {
-    [SecurityHeaders]
-    public class Index : PageModel
+    private readonly IIdentityServerInteractionService _interaction;
+    private readonly IWebHostEnvironment _environment;
+        
+    public ViewModel View { get; set; }
+        
+    public Index(IIdentityServerInteractionService interaction, IWebHostEnvironment environment)
     {
-        private readonly IIdentityServerInteractionService _interaction;
-        private readonly IWebHostEnvironment _environment;
+        _interaction = interaction;
+        _environment = environment;
+    }
         
-        public ViewModel View { get; set; }
-        
-        public Index(IIdentityServerInteractionService interaction, IWebHostEnvironment environment)
-        {
-            _interaction = interaction;
-            _environment = environment;
-        }
-        
-        public async Task OnGet(string errorId)
-        {
-            View = new ViewModel();
+    public async Task OnGet(string errorId)
+    {
+        View = new ViewModel();
 
-            // retrieve error details from identityserver
-            var message = await _interaction.GetErrorContextAsync(errorId);
-            if (message != null)
+        // retrieve error details from identityserver
+        var message = await _interaction.GetErrorContextAsync(errorId);
+        if (message != null)
+        {
+            View.Error = message;
+
+            if (!_environment.IsDevelopment())
             {
-                View.Error = message;
-
-                if (!_environment.IsDevelopment())
-                {
-                    // only show in development
-                    message.ErrorDescription = null;
-                }
+                // only show in development
+                message.ErrorDescription = null;
             }
         }
     }
