977416e66526f5e2d2541713f5c4cbd0fe7db36c
Joe DeCock <joe.decock@hey.com>
2022-01-11 14:47:07 -0600

    Quickstart 2 - Add logout controller

--- IdentityServer/v6/Quickstarts/2_InteractiveAspNetCore/src/MvcClient/Controllers/HomeController.cs
+++ IdentityServer/v6/Quickstarts/2_InteractiveAspNetCore/src/MvcClient/Controllers/HomeController.cs
@@ -28,4 +28,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Logout()
+    {
+        return SignOut("Cookies", "oidc");
+    }
 }
