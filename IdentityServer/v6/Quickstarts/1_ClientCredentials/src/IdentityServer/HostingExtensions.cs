13a46ccefddb91c1df1707153044d01cc5b53c07
Joe DeCock <joe.decock@hey.com>
2022-01-11 12:20:18 -0600

    Add back the commented out ui to quickstart 1
    For people who are starting with quickstart 2 and using the quickstart
    1 code here as a starting point

--- IdentityServer/v6/Quickstarts/1_ClientCredentials/src/IdentityServer/HostingExtensions.cs
+++ IdentityServer/v6/Quickstarts/1_ClientCredentials/src/IdentityServer/HostingExtensions.cs
@@ -9,6 +9,9 @@ internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
+        // uncomment, if you want to add an MVC-based UI
+        //services.AddRazorPages();
+
         builder.Services.AddIdentityServer()
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients);
@@ -18,12 +21,21 @@ internal static class HostingExtensions
     
     public static WebApplication ConfigurePipeline(this WebApplication app)
     { 
+        // uncomment if you want to add a UI
+        //app.UseStaticFiles();
+        //app.UseRouting();
+
         app.UseSerilogRequestLogging();
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
         app.UseIdentityServer();
+
+        // uncomment, if you want to add a UI
+        //app.UseAuthorization();
+        //app.MapRazorPages().RequireAuthorization();
+
         return app;
     }
 }
