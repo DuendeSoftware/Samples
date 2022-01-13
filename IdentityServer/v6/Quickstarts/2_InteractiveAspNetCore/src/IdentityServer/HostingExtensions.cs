86bc774bd3e1bd00c399d52de326bad9d6a9fd02
Joe DeCock <joe.decock@hey.com>
2022-01-13 15:15:59 -0600

    Quickstart 2 - Cloud hosted external OP

--- IdentityServer/v6/Quickstarts/2_InteractiveAspNetCore/src/IdentityServer/HostingExtensions.cs
+++ IdentityServer/v6/Quickstarts/2_InteractiveAspNetCore/src/IdentityServer/HostingExtensions.cs
@@ -1,5 +1,6 @@
 using Duende.IdentityServer;
 using IdentityServerHost;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 namespace IdentityServer;
@@ -23,6 +24,23 @@ internal static class HostingExtensions
 
                 options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
                 options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+            })
+            .AddOpenIdConnect("oidc", "Demo IdentityServer", options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+                options.SaveTokens = true;
+
+                options.Authority = "https://demo.duendesoftware.com";
+                options.ClientId = "interactive.confidential";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
             });
 
         return builder.Build();
