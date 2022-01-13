e6c1d598b0e8682f5864bfdf3ab5a65c9a9cb037
Joe DeCock <joe.decock@hey.com>
2022-01-13 14:14:43 -0600

    Quickstart 2 - Google auth

--- IdentityServer/v6/Quickstarts/2_InteractiveAspNetCore/src/IdentityServer/HostingExtensions.cs
+++ IdentityServer/v6/Quickstarts/2_InteractiveAspNetCore/src/IdentityServer/HostingExtensions.cs
@@ -1,3 +1,4 @@
+using Duende.IdentityServer;
 using IdentityServerHost;
 using Serilog;
 
@@ -14,6 +15,15 @@ internal static class HostingExtensions
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients)
             .AddTestUsers(TestUsers.Users);
+
+        builder.Services.AddAuthentication()
+            .AddGoogle("Google", options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+            });
 
         return builder.Build();
     }
