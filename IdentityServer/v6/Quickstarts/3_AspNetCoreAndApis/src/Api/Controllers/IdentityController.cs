a09a5c4cf48567869dd829888b684ad74fcdee54
Joe DeCock <joe.decock@hey.com>
2022-01-14 09:39:35 -0600

    Quickstart 3 - Begin from quickstart 2 code

--- IdentityServer/v6/Quickstarts/3_AspNetCoreAndApis/src/Api/Controllers/IdentityController.cs
+++ IdentityServer/v6/Quickstarts/3_AspNetCoreAndApis/src/Api/Controllers/IdentityController.cs
@@ -1,20 +1,14 @@
-﻿// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
-
-
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
+namespace Api.Controllers;
+
+[Route("identity")]
+[Authorize]
+public class IdentityController : ControllerBase
 {
-    [Route("identity")]
-    [Authorize]
-    public class IdentityController : ControllerBase
+    public IActionResult Get()
     {
-        public IActionResult Get()
-        {
-            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
-        }
+        return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
     }
 }
