a09253b368bd954eab95f82a3da77fabae54d286
Joe DeCock <joe.decock@hey.com>
2022-01-14 16:17:51 -0600

    Quickstart 4 - Begin from quickstart 3

--- IdentityServer/v6/Quickstarts/4_EntityFramework/src/Client/Program.cs
+++ IdentityServer/v6/Quickstarts/4_EntityFramework/src/Client/Program.cs
@@ -1,62 +1,50 @@
-﻿// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
-
-
+﻿using System.Text.Json;
 using IdentityModel.Client;
-using System;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
 
-namespace Client
+// discover endpoints from metadata
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
 {
-    public class Program
-    {
-        private static async Task Main()
-        {
-            // discover endpoints from metadata
-            var client = new HttpClient();
+    Console.WriteLine(disco.Error);
+    return;
+}
 
-            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
-            if (disco.IsError)
-            {
-                Console.WriteLine(disco.Error);
-                return;
-            }
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
 
-            // request token
-            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
-            {
-                Address = disco.TokenEndpoint,
-                ClientId = "client",
-                ClientSecret = "secret",
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1",
+});
 
-                Scope = "api1"
-            });
-            
-            if (tokenResponse.IsError)
-            {
-                Console.WriteLine(tokenResponse.Error);
-                return;
-            }
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    Console.WriteLine(tokenResponse.ErrorDescription);
+    return;
+}
 
-            Console.WriteLine(tokenResponse.Json);
-            Console.WriteLine("\n\n");
+Console.WriteLine(tokenResponse.AccessToken);
 
-            // call api
-            var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
 
-            var response = await apiClient.GetAsync("https://localhost:6001/identity");
-            if (!response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(response.StatusCode);
-            }
-            else
-            {
-                var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
-                Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
-            }
-        }
-    }
+// call api
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:6001/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var content = await response.Content.ReadAsStringAsync();
+
+    var parsed = JsonDocument.Parse(content);
+    var formatted = JsonSerializer.Serialize(parsed, new JsonSerializerOptions { WriteIndented = true });
+
+    Console.WriteLine(formatted);
 }
--- IdentityServer/v6/Quickstarts/4_EntityFramework/src/MvcClient/Program.cs
+++ IdentityServer/v6/Quickstarts/4_EntityFramework/src/MvcClient/Program.cs
@@ -1,30 +1,53 @@
-// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication;
 
+var builder = WebApplication.CreateBuilder(args);
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-namespace MvcClient
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthentication(options =>
 {
-    public class Program
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        options.Authority = "https://localhost:5001";
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+        options.ClientId = "mvc";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        
+        options.SaveTokens = true;
+
+        options.Scope.Add("api1");
+        options.Scope.Add("offline_access");
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .RequireAuthorization();
+
+app.Run();
