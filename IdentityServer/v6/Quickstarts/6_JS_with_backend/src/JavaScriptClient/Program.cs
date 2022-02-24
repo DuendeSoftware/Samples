c1c2a2d1d2ad408d972f298a2b7448b0ab799dc9
Joe DeCock <joe.decock@hey.com>
2022-02-24 10:40:04 -0600

    Quickstart 6 - Pipeline and services

--- IdentityServer/v6/Quickstarts/6_JS_with_backend/src/JavaScriptClient/Program.cs
+++ IdentityServer/v6/Quickstarts/6_JS_with_backend/src/JavaScriptClient/Program.cs
@@ -1,6 +1,52 @@
+using System.IdentityModel.Tokens.Jwt;
+using Duende.Bff.Yarp;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services
+    .AddBff()
+    .AddRemoteApis();
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.ClientId = "bff";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.Scope.Add("api1");
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+
+app.UseBff();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBffManagementEndpoints();
+});
 app.Run();
