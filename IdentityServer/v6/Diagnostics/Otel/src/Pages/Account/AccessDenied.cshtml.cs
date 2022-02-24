48da077d3038603e0226d544682c51eaa968108d
Dominick Baier <dbaier@leastprivilege.com>
2022-02-24 11:10:11 +0100

    first cut

---
+++ IdentityServer/v6/Diagnostics/Otel/src/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Otel.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
