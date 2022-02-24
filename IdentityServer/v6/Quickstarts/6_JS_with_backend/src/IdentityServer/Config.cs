cece66fbff5b794b93a4287887aba80e6515c0f0
Joe DeCock <joe.decock@hey.com>
2022-02-24 10:44:14 -0600

    Quickstart 6 - Configure BFF client

--- IdentityServer/v6/Quickstarts/6_JS_with_backend/src/IdentityServer/Config.cs
+++ IdentityServer/v6/Quickstarts/6_JS_with_backend/src/IdentityServer/Config.cs
@@ -68,6 +68,27 @@ public static class Config
                     IdentityServerConstants.StandardScopes.Profile,
                     "api1"
                 }
+            },
+            // JavaScript BFF client
+            new Client
+            {
+                ClientId = "bff",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5003/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5003/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1"
+                }
             }
         };
 }
