ca305d87b74f8536fd622562a5d07a33df527087
Joe DeCock <joe.decock@hey.com>
2022-02-25 09:40:25 -0600

    Quickstart 6 - Remove HostingExtensions
    This makes it simpler in the quickstart because we don't need to create as
    many files.

--- IdentityServer/v6/Quickstarts/6_JS_with_backend/src/JavaScriptClient/Program.cs
+++ IdentityServer/v6/Quickstarts/6_JS_with_backend/src/JavaScriptClient/Program.cs
@@ -1,27 +1,64 @@
-using JavaScriptClient;
-using Serilog;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Duende.Bff.Yarp;
+using Microsoft.AspNetCore.Authorization;
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateBootstrapLogger();
+var builder = WebApplication.CreateBuilder(args);
 
-Log.Information("Starting up");
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+builder.Services.AddAuthorization();
 
-try
+builder.Services
+    .AddBff()
+    .AddRemoteApis();
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.ClientId = "bff";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.Scope.Add("api1");
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+    });
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
 {
-    var builder = WebApplication.CreateBuilder(args);
-    var app = builder
-        .ConfigureServices()
-        .ConfigurePipeline();
+    app.UseDeveloperExceptionPage();
+}
 
-    app.Run();
-}
-catch (Exception ex)
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+
+app.UseBff();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
 {
-    Log.Fatal(ex, "Unhandled exception");
-}
-finally
-{
-    Log.Information("Shut down complete");
-    Log.CloseAndFlush();
-}
+    endpoints.MapGet("/local/identity", [Authorize] (ClaimsPrincipal user) =>
+    {
+        var name = user.FindFirst("name")?.Value ?? user.FindFirst("sub")?.Value;
+        return Results.Json(new { message = "Local API Success!", user = name });
+    }).AsBffApiEndpoint();
+
+    endpoints.MapBffManagementEndpoints();
+
+    endpoints.MapRemoteBffApiEndpoint("/remote", "https://localhost:6001")
+        .RequireAccessToken(Duende.Bff.TokenType.User);
+});
+
+app.Run();
