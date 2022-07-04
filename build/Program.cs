4441a64a3c31e6eb773d70ab634e11c6724726f4
Dominick Baier <dbaier@leastprivilege.com>
2022-07-04 09:54:50 +0200

    update build script

--- build/Program.cs
+++ build/Program.cs
@@ -13,10 +13,11 @@ namespace build
             Target("build", () =>
             {
                 var files = Directory.GetFiles(".", "*.sln", SearchOption.AllDirectories)
-                    .Where(f => !f.Contains("webform", StringComparison.OrdinalIgnoreCase));
+                    .Where(f => !f.StartsWith("./various", StringComparison.OrdinalIgnoreCase));
 
                 foreach (var file in files)
                 {
+                    Console.WriteLine($"building {file}");
                     Run("dotnet", $"build {file} -c Release --nologo");
                 }
             });
