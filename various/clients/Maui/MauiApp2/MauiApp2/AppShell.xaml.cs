e186d9575360d45f31b9fbcebeacafc117179b66
Dominick Baier <dbaier@leastprivilege.com>
2022-07-04 09:48:24 +0200

    add Maui sample

---
+++ various/clients/Maui/MauiApp2/MauiApp2/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp2;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ various/clients/Maui/MauiApp2/MauiApp2/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using IdentityModel.OidcClient;
+
+namespace MauiApp2;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // add main page
+        builder.Services.AddSingleton<MainPage>();
+
+        // setup OidcClient
+        builder.Services.AddSingleton(new OidcClient(new()
+        {
+            Authority = "https://demo.duendesoftware.com",
+
+            ClientId = "interactive.public",
+            Scope = "openid profile api",
+            RedirectUri = "myapp://callback",
+
+            Browser = new MauiAuthenticationBrowser()
+        }));
+
+        return builder.Build();
+    }
+}
