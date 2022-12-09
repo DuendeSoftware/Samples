65920b7f5b1ee6edc3cef079cb5b9db96d645ccc
Joe DeCock <joe.decock@hey.com>
2022-12-09 12:12:22 -0600

    Wire up login

--- IdentityServer/v6/BFF/AngularBffSample/src/FrontendHost/ClientApp/proxy.conf.js
+++ IdentityServer/v6/BFF/AngularBffSample/src/FrontendHost/ClientApp/proxy.conf.js
@@ -5,9 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 const PROXY_CONFIG = [
   {
-    context: [
-      "/weatherforecast",
-   ],
+    context: ["/todos", "/bff", "/signin-oidc", "/signout-callback-oidc"],
     target: target,
     secure: false,
     headers: {
