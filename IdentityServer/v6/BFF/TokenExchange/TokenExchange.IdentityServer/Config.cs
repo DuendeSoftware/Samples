77d4755e6e84d17c33dbd5062e6af1c2e7bdb88d
Joe DeCock <josephdecock@gmail.com>
2023-06-22 20:20:15 -0500

    Added BFF token exchange/ITokenRetriever sample

---
+++ IdentityServer/v6/BFF/TokenExchange/TokenExchange.IdentityServer/Config.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace TokenExchange.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("api", new[] { "name" })
+            };
+
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "spa",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    
+                    AllowedGrantTypes = 
+                    { 
+                        GrantType.AuthorizationCode, 
+                        GrantType.ClientCredentials,
+                        OidcConstants.GrantTypes.TokenExchange 
+                    },
+
+                    RedirectUris = { "https://localhost:6001/signin-oidc" },
+                    
+                    BackChannelLogoutUri = "https://localhost:6001/bff/backchannel",
+                    
+                    PostLogoutRedirectUris = { "https://localhost:6001/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "api" },
+                },
+            };
+    }
+}
