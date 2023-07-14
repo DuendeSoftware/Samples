bd600b3baa24cc919bf86bb8d930d54490445d50
Joe DeCock <josephdecock@gmail.com>
2023-07-14 11:21:05 -0500

    Added solution for DCR with software statement

---
+++ IdentityServer/v6/Configuration/SoftwareStatement/ConsoleDcrClient/Constants.cs
@@ -0,0 +1,7 @@
+namespace ConsoleDcrClient;
+
+public class Constants
+{
+    public const string Authority = "https://localhost:5001";
+    public const string SimpleApi = "https://localhost:6001/";
+}
---
+++ IdentityServer/v6/Configuration/SoftwareStatement/IdentityServer/Pages/Admin/ApiScopes/Edit.cshtml.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServer.Pages.Admin.ApiScopes;
+
+[SecurityHeaders]
+[Authorize]
+public class EditModel : PageModel
+{
+    private readonly ApiScopeRepository _repository;
+
+    public EditModel(ApiScopeRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [BindProperty]
+    public ApiScopeModel InputModel { get; set; } = default!;
+    [BindProperty]
+    public string? Button { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(string id)
+    {
+        var apiScope = await _repository.GetByIdAsync(id);
+        if (apiScope == null)
+        {
+            return RedirectToPage("/Admin/ApiScopes/Index");
+        }
+        else
+        {
+            InputModel = apiScope;
+            return Page();
+        }
+    }
+
+    public async Task<IActionResult> OnPostAsync(string id)
+    {
+        if (Button == "delete")
+        {
+            await _repository.DeleteAsync(id);
+            return RedirectToPage("/Admin/ApiScopes/Index");
+        }
+
+        if (ModelState.IsValid)
+        {
+            await _repository.UpdateAsync(InputModel);
+            return RedirectToPage("/Admin/ApiScopes/Edit", new { id });
+        }
+
+        return Page();
+    }
+}
