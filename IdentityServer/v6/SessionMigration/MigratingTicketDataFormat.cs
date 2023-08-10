316018c5db15e3ada4967a1e7519c51dde647f1d
Anders Abel <anders@abel.nu>
2023-08-10 20:33:54 +0200

    Review fixes - Prevent duplicate server side sessions on race. - Copy all code for cookie expiration.

--- IdentityServer/v6/SessionMigration/MigratingTicketDataFormat.cs
+++ IdentityServer/v6/SessionMigration/MigratingTicketDataFormat.cs
@@ -1,4 +1,6 @@
-﻿using Duende.IdentityServer.Stores;
+﻿using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Stores;
+using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Security.Claims;
@@ -51,25 +53,79 @@ public class MigratingTicketDataFormat : ISecureDataFormat<AuthenticationTicket>
         // context there is no risk for dead locks.
         var sessionId = sessionStore.StoreAsync(ticket).Result;
 
-        var principal = new ClaimsPrincipal(
-            new ClaimsIdentity(
-                new[] { new Claim(SessionIdClaim, sessionId, ClaimValueTypes.String, options.ClaimsIssuer) },
-                options.ClaimsIssuer));
-        
-        ticket = new AuthenticationTicket(principal, null, scheme);
+        // There's a potential race condition where two requests could migrate the same session. Check
+        // if there's another entry with the same SID and if it is rollback the one we created and
+        // don't alter the cookie.
+        if (HasDuplicate(sessionStore, ticket))
+        {
+            sessionStore.RemoveAsync(sessionId).Wait();
+        }
+        else
+        {
+            var principal = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new[] { new Claim(SessionIdClaim, sessionId, ClaimValueTypes.String, options.ClaimsIssuer) },
+                    options.ClaimsIssuer));
 
-        var cookieValue = inner.Protect(ticket, purpose);
+            ticket = new AuthenticationTicket(principal, null, scheme);
 
+            var cookieValue = inner.Protect(ticket, purpose);
+
+            var cookieOptions = CreateCookieOptions(ticket, context);
+
+            options.CookieManager.AppendResponseCookie(
+                context,
+                options.Cookie.Name!,
+                cookieValue,
+                cookieOptions);
+        }
+        return ticket;
+    }
+
+    private CookieOptions CreateCookieOptions(AuthenticationTicket ticket, HttpContext context)
+    {
         // Cookie option generation copied from cookie handler.
+
         var cookieOptions = options.Cookie.Build(context);
         cookieOptions.Expires = null;
 
-        options.CookieManager.AppendResponseCookie(
-            context,
-            options.Cookie.Name!,
-            cookieValue,
-            cookieOptions);
+        if (ticket.Properties.IsPersistent)
+        {
+            DateTimeOffset issuedUtc;
+            if (ticket.Properties.IssuedUtc.HasValue)
+            {
+                issuedUtc = ticket.Properties.IssuedUtc.Value;
+            }
+            else
+            {
+                issuedUtc = DateTime.UtcNow;
+                ticket.Properties.IssuedUtc = issuedUtc;
+            }
 
-        return ticket;
+            if (!ticket.Properties.ExpiresUtc.HasValue)
+            {
+                ticket.Properties.ExpiresUtc = issuedUtc.Add(options.ExpireTimeSpan);
+            }
+
+            var expiresUtc = ticket.Properties.ExpiresUtc ?? issuedUtc.Add(options.ExpireTimeSpan);
+            cookieOptions.Expires = expiresUtc.ToUniversalTime();
+        }
+
+        return cookieOptions;
+    }
+
+    private bool HasDuplicate(IServerSideTicketStore sessionStore, AuthenticationTicket ticket)
+    {
+        var sid = ticket.GetSessionId();
+
+        var filter = new SessionQuery
+        {
+            SessionId = sid
+        };
+
+        var sessions = sessionStore.QuerySessionsAsync(filter).Result;
+
+        // There should be only one entry, the one we just created.
+        return sessions.Results.Count > 1;
     }
 }
