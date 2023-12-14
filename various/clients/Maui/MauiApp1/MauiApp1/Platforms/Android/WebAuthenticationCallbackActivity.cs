e7d83ce76d4904162cfd3fd680e6ea54933c81dd
Brock Allen <brockallen@gmail.com>
2023-12-14 12:03:08 -0500

    add updated maui sample and include andriod

---
+++ various/clients/Maui/MauiApp1/MauiApp1/Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content.PM;
+
+namespace YourNameSpace;
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(new[] { Android.Content.Intent.ActionView },
+              Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+              DataScheme = CALLBACK_SCHEME)]
+public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+{
+    const string CALLBACK_SCHEME = "myapp";
+
+}
