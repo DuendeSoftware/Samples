ccfc5437da86e2a93b480fb469a9c818b510f5d8
Joe DeCock <josephdecock@gmail.com>
2024-01-19 11:21:37 -0600

    PAT - Update to .NET 8

---
+++ IdentityServer/v7/PAT/IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
