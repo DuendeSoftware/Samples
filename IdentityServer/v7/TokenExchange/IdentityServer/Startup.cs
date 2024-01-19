0b06d32a68ec513c34ad54e5dbd304f3aa479ef2
Joe DeCock <josephdecock@gmail.com>
2024-01-19 12:54:15 -0600

    TokenExchange - Update to .NET 8

---
+++ IdentityServer/v7/TokenExchange/IdentityServer/Startup.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        Console.Title = "IdentityServer";
+
+        var builder = services.AddIdentityServer()
+            .AddInMemoryApiScopes(Config.Scopes)
+            .AddInMemoryClients(Config.Clients);
+
+        // registers extension grant validator for the token exchange grant type
+        builder.AddExtensionGrantValidator<TokenExchangeGrantValidator>();
+        
+        // register a profile service to emit the act claim
+        builder.AddProfileService<ProfileService>();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+        
+        app.UseIdentityServer();
+    }
+}
