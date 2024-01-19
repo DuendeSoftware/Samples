20d29906b09fd1e9197d90a930012069fd3a549c
Joe DeCock <josephdecock@gmail.com>
2024-01-19 11:25:45 -0600

    DPoP - Fix API logging

--- IdentityServer/v7/DPoP/Api/Program.cs
+++ IdentityServer/v7/DPoP/Api/Program.cs
@@ -1,5 +1,4 @@
 using Serilog;
-using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
 
 namespace Api;
@@ -8,7 +7,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.Title = "DPoP API Host";
+        Console.Title = "API";
 
         BuildWebHost(args).Run();
     }
@@ -16,10 +15,7 @@ public class Program
     public static IHost BuildWebHost(string[] args)
     {
         Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Verbose()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-            .MinimumLevel.Override("System", LogEventLevel.Warning)
-            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+            .MinimumLevel.Information()
             .Enrich.FromLogContext()
             .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
             .CreateLogger();
