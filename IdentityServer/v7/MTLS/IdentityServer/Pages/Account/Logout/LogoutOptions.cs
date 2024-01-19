24e841a962ad97525c2d8dcebe4a5e98b4d6975a
Joe DeCock <josephdecock@gmail.com>
2024-01-19 16:22:48 -0600

    mTLS - Switch back to IdentityServerHost

--- IdentityServer/v7/MTLS/IdentityServer/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace IdentityServer.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
---
+++ IdentityServer/v7/MTLS/IdentityServerHost/Clients.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+using Duende.IdentityServer;
+
+namespace IdentityServerHost
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> List =>
+            new []
+            {
+                new Client
+                {
+                    ClientId = "mtls",
+
+                    ClientSecrets =
+                    {
+                        new Secret("5D9E9B6B333CD42C99D1DE6175CC0F3EF99DDF68")
+                        {
+                            Type = IdentityServerConstants.SecretTypes.X509CertificateThumbprint
+                        },
+                    },
+
+                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+
+                    RedirectUris = { "https://localhost:44301/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44301/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44301/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope1" }
+                },           
+            };
+    }
+}
