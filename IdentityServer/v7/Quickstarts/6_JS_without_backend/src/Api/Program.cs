87d71eb7d681fbfd611cf3d473392de84d22c974
Joe DeCock <josephdecock@gmail.com>
2024-01-26 16:31:04 -0600

    qs6 (without bff) - update to v7

--- IdentityServer/v7/Quickstarts/6_JS_without_backend/src/Api/Program.cs
+++ IdentityServer/v7/Quickstarts/6_JS_without_backend/src/Api/Program.cs
@@ -1,30 +1,25 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-using Microsoft.IdentityModel.Tokens;
+
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication("Bearer")
+builder.Services.AddAuthentication()
     .AddJwtBearer(options =>
     {
         options.Authority = "https://localhost:5001";
         options.TokenValidationParameters.ValidateAudience = false;
     });
 builder.Services.AddAuthorization(options =>
+{
     options.AddPolicy("ApiScope", policy =>
     {
         policy.RequireAuthenticatedUser();
         policy.RequireClaim("scope", "api1");
-    })
-);
-
+    });
+});
 builder.Services.AddCors(options =>
 {
     // this defines a CORS policy called "default"
@@ -38,20 +33,10 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
 app.UseCors("default");
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers().RequireAuthorization("ApiScope");
-
+app.MapGet("identity", (ClaimsPrincipal user) => user.Claims.Select(c => new { c.Type, c.Value }))
+    .RequireAuthorization("ApiScope");
+    
 app.Run();
--- IdentityServer/v7/Quickstarts/6_JS_without_backend/src/IdentityServer/Pages/Consent/InputModel.cs
+++ IdentityServer/v7/Quickstarts/6_JS_without_backend/src/IdentityServer/Pages/Consent/InputModel.cs
@@ -1,16 +1,13 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-
-using System.Collections.Generic;
-
-namespace IdentityServerHost.Pages.Consent;
+namespace IdentityServer.Pages.Consent;
 
 public class InputModel
 {
-    public string Button { get; set; }
-    public IEnumerable<string> ScopesConsented { get; set; }
+    public string? Button { get; set; }
+    public IEnumerable<string> ScopesConsented { get; set; } = new List<string>();
     public bool RememberConsent { get; set; } = true;
-    public string ReturnUrl { get; set; }
-    public string Description { get; set; }
+    public string? ReturnUrl { get; set; }
+    public string? Description { get; set; }
 }
