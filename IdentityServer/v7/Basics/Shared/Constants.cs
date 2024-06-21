5b887dcac0dd4e6751894c7c4f0e5d6d3b20d802
Roland Guijt <roland.guijt@gmail.com>
2024-06-21 13:27:12 +0200

    File based namespace + HostingExtensions pattern for idsvr host

--- IdentityServer/v7/Basics/Shared/Constants.cs
+++ IdentityServer/v7/Basics/Shared/Constants.cs
@@ -1,13 +1,12 @@
-namespace Client
+namespace Client;
+
+public class Urls
 {
-    public class Urls
-    {
-        public const string IdentityServer = "https://localhost:5001";
-        public const string SampleApi = "https://localhost:5002/";
-    
-        // The API built with the OWIN pipeline defaults to not using TLS
-        // so that the sample will run "out of the box", without needing
-        // to create certificates and manage bindings with netsh.
-        public const string SampleOwinApi = "http://localhost:5003/";
-    }
+    public const string IdentityServer = "https://localhost:5001";
+    public const string SampleApi = "https://localhost:5002/";
+
+    // The API built with the OWIN pipeline defaults to not using TLS
+    // so that the sample will run "out of the box", without needing
+    // to create certificates and manage bindings with netsh.
+    public const string SampleOwinApi = "http://localhost:5003/";
 }
