b5854354b2444b6c0b65fd9b0dd9b61aad9e3f5a
Roland Guijt <roland.guijt@gmail.com>
2024-07-22 18:15:37 +0200

    Remove parenthesis

--- IdentityServer/v7/Quickstarts/3_AspNetCoreAndApis/src/WebClient/Pages/CallApi.cshtml.cs
+++ IdentityServer/v7/Quickstarts/3_AspNetCoreAndApis/src/WebClient/Pages/CallApi.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace MyApp.Namespace
 {
 
-    public class CallApiModel() : PageModel
+    public class CallApiModel : PageModel
     {
         public string Json = string.Empty;
 
